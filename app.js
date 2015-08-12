e676a905a9aa1b0f351965ae213e6bab6adbb547
Tait <tait@TaitdeMac-mini.local>
2015-08-12 18:45:54 +0800

    update

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ var topGaniersLink = 'http://finance.yahoo.com/_remote/?m_id=MediaRemoteInstance
 	stockCenterLink = 'http://finance.yahoo.com/stock-center/',
 	earningsCalendarLink = 'http://biz.yahoo.com/research/earncal/20150831.html';
 
-app.get("/", function(req, res, next){
-	//取出指定日期将发布财报的公司名称和其股票代码 （数据来源，yahoo finance)
-	getReportCompanys();
-});
-
-var getReportCompanys = function(){
+var getReportCompanys = function(req, res, next){
 	superagent.get(earningsCalendarLink)
 		.end(function(err,sres){
 			if (err) {
@@ -45,6 +40,67 @@ var getReportCompanys = function(){
 			res.send(items);
 		});
 }
+
+var getTopStock = function(req, res, next){
+	
+	superagent.get(stockCenterLink)
+	.end(function(err,sres){
+		console.log('in end method...')
+		if (err) {
+			return next(err);
+		};
+
+		var $ = cheerio.load(sres.text);
+		console.log('sres.text loaded...')
+		//第三个p标签的第一个table
+		var table = $('.yom-data .col-8 .phatable').html();
+
+
+		/*var items = [];
+
+		//遍历行，提取公司名和股票代码
+		$(table).find('tr').each(function(idx,element){
+			// console.log('........................行内容:', $(element).text());
+			if (idx==0 || idx==1) return true;
+			var td = $(element).find('td'),
+			 	companyName = $(td).eq(0).text(),
+			 	stockCode = $(td).eq(1).text();
+
+			if (companyName.trim()=='') return true;
+			items.push({
+				company:companyName,
+				stock:stockCode
+			});
+			
+		});*/
+		res.send(table);
+	});
+}
+app.get("/", function(req, res, next){
+	//取出指定日期将发布财报的公司名称和其股票代码 （数据来源，yahoo finance)
+	// var reportList = getReportCompanys(req, res, next);
+	getTopStock(req, res, next);
+});
+
 app.listen(3000, function(){
 	console.log('app is listening at port 3000');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
