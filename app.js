513093a8d0ef379cb86ba9bbc769f3763e32b682
goldcoast <goldcoast0448@gmail.com>
2015-08-19 09:06:54 +0800

    updated

--- app.js
+++ app.js
@@ -12,14 +12,30 @@ Date.prototype.addHours= function(h){
 var today = (new Date()).toISOString().slice(0, 10).replace(/-/g,''),
 	reportDay = (new Date().addHours(-12)).toISOString().slice(0, 10).replace(/-/g,'');
 
-var topLength = 20;
+var topLength = 50,
+	TIME_OUT = 5000, //5S
 	topGaniersLink = 'http://finance.yahoo.com/_remote/?m_id=MediaRemoteInstance&instance_id=85ac7b2b-640f-323f-a1c1-00b2f4865d18&mode=xhr&ctab=tab2&nolz=1&count='+topLength+'&start=0&category=percentagegainers&no_tabs=1',
 	topLosersLink  = 'http://finance.yahoo.com/_remote/?m_id=MediaRemoteInstance&instance_id=85ac7b2b-640f-323f-a1c1-00b2f4865d18&mode=xhr&ctab=tab3&nolz=1&count='+topLength+'&start=0&category=percentagelosers&no_tabs=1',
 	stockCenterLink = 'http://finance.yahoo.com/stock-center/',
 	earningsCalendarLink = 'http://biz.yahoo.com/research/earncal/'+reportDay+'.html';
 
 var reportFile = 'earningsCalendar_'+reportDay+'.json',
-	ganiersFile = ''
+	ganiersFile = '';
+var fileType = {
+		ganier: 'ganier',
+		loser: 'loser'
+	};
+
+var topLink = {
+		ganier: topGaniersLink,
+		loser: topLosersLink
+	}
+
+var fileName = {
+	reported: 'earningsCalendar_',
+	ganier: 'topdata_ganier_',
+	loser: 'topdata_loser_'
+}
 
 
 var createFolder = function(reportDate){
@@ -43,26 +59,46 @@ var writeData = function(fileName, data, reportDate){
 	}
 }
 
+var readData = function(fileName, reportDate){
+	var filePath = './'+reportDate+'/'+fileName;
+	console.log('filePath', filePath);
+	if (fs.existsSync(filePath)) {
+		return JSON.parse(fs.readFileSync(filePath));
+	}else{
+		return undefined;
+	}
+}
 //filter the symbol which we need 
-var filterSymbol = function(reportData, topData, finalList, res){
-		console.log("...... filterSymbol start ")
-		for (var i = reportData.length - 1; i >= 0; i--) {
-			report = reportData[i];
-			for (var j = topData.length - 1; j >= 0; j--) {
-				top = topData[j];
-				if (top.symbol === report.symbol) {
-					console.log('top.symbol', j, top.symbol);
-					finalList.push(top);
-				};
+var filterSymbol = function(){
+	console.log("...... filterSymbol start ")
+	var ganierList = readData(fileName.ganier+reportDay+'.json', reportDay),
+		loserList = readData(fileName.loser+reportDay+'.json', reportDay),
+		reportedList = readData(fileName.reported+reportDay+'.json', reportDay),
+		finalList = [];
+
+	// console.log('ganier list: ', ganierList);
+	// console.log('------------------------------------------------------------');
+	// console.log('reportedList list: ', reportedList);
+
+	for (var i = reportedList.length - 1; i >= 0; i--) {
+		var report = reportedList[i];
+		for (var j = loserList.length - 1; j >= 0; j--) {
+			var top = loserList[j];
+			if (top.symbol === report.stock) {
+				console.log('top.symbol', j, top.symbol);
+				finalList.push(top);
 			};
 		};
-		console.log("...... done finalList is ", finalList)
-		res.send(finalList);
+	};
+	console.log("...... done finalList is ", finalList)
+	// res.send(finalList);
+	writeData('result.json', finalList, reportDay);
 }
 // get the top symbol data
-var fetchTopStock = function(req, res, next){
+var fetchTopStock = function(req, res, next, dataLink, dataType){
 	console.log('accessing topGaniersLink: ', topGaniersLink);
-	superagent.get(topGaniersLink)
+	superagent.get(dataLink)
+		.timeout(TIME_OUT)
 		.end(function(err,sres){
 			if (err) {
 				console.error(' excpetion occured.... ')
@@ -101,7 +137,7 @@ var fetchTopStock = function(req, res, next){
 				
 			});
 
-			writeData('topdata_'+reportDay+'.json', topList,reportDay);
+			writeData('topdata_'+dataType+'_'+reportDay+'.json', topList,reportDay);
 
 			// fs.writeFileSync('./topdata_'+reportDay+'.json',JSON.stringify(topList));
 
@@ -110,19 +146,21 @@ var fetchTopStock = function(req, res, next){
 			console.info('getTopStock each done .. and write file done');
 
 			// filterSymbol(reportList, topList, finalList, res);
-			res.send(topList);
+			// res.send('top length: '+topList.length);
 	});
 }
 
-//暂时只取top100的，等这个完成再加其它的。
+//暂时只取top50的，等这个完成再加其它的。
 /*var getTopData = function(){
-	var topArray = [topGaniersLink, topLosersLink];
-	var JsonObj=JSON.parse(fs.readFileSync('./output.json'));
-}*/
-//取出指定日期将发布财报的公司名称和其股票代码 （数据来源，yahoo finance)
-var getReportCompanys = function(req, res, next, reportList){
+
+
+	// var JsonObj=JSON.parse(fs.readFileSync('./output.json'));
+}
+*///取出指定日期将发布财报的公司名称和其股票代码 （数据来源，yahoo finance)
+var getReportCompanys = function(req, res, next){
 		console.log('accessing earningsCalendarLink: ', earningsCalendarLink);
 		superagent.get(earningsCalendarLink)
+			.timeout(TIME_OUT)
 			.end(function(err,sres){
 				if (err) {
 					return next(err);
@@ -132,8 +170,8 @@ var getReportCompanys = function(req, res, next, reportList){
 				//第三个p标签的第一个table
 				var table = $('p').eq(3).find('table').first().html();
 
-				console.log('getReportCompanys each start ..')
-				
+				// console.log('getReportCompanys each start ..')
+				var reportList = [];
 				//遍历行，提取公司名和股票代码
 				$(table).find('tr').each(function(idx,element){
 					
@@ -146,21 +184,17 @@ var getReportCompanys = function(req, res, next, reportList){
 
 					reportList.push({
 						company:companyName,
-						stock:symbol
+						symbol:symbol
 					});
 					
 				});
 
 				writeData('earningsCalendar_'+reportDay+'.json', reportList, reportDay);
-				// topLength = reportList.length;
-				// if (topLength> 50) {
-				// 	topLength  = 30;
-				// };
 				console.log( '****** reportList.length', reportList.length);
 				// topGaniersLink = topGaniersLink.replace('topLength', topLength);
 				// console.log('getReportCompanys each done ..')
 
-				// res.send(items);
+				res.send( '****** reportList.length: '+ reportList.length);
 			});
 	}
 
@@ -169,21 +203,16 @@ var getReportCompanys = function(req, res, next, reportList){
 app.get("/", function(req, res, next){
 	console.info('..................................start .............................')
 	createFolder(reportDay);
-	// var reportList = [];
-	// getTopStock(req, res, next);
-	// getReportCompanys(req,res, next, reportList);
 
+	// getReportCompanys(req,res, next);
 
-	fetchTopStock(req, res, next);
+	// fetchTopStock(req, res, next, topLink.loser, fileType.loser);
 
-	// if (reportList.length>20) {
-		//todo 如果长度为 38 将长度分解为数组 [10,20,30, 38]类似的
-		//再遍历
-	// };
+	filterSymbol();
 
-	// fs.mkdirSync('./test_'+reportDay,'777');   
 	console.info('..................................finished .............................')
 	console.info('')
+	res.send('done!');
 });
 
 app.listen(3000, function(){
